4a155f064ae69e2b5593dce2fe4b0439fc9f0790
Gleb Grishakov <glebson1988@yandex.ru>
2020-07-09 17:50:05 +0300

    upgrade ruby to 2.7.1 and rails to 6.0.3

--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'boot'
-
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'action_cable/engine'
-# require "sprockets/railtie"
-require 'rails/test_unit/railtie'
+require 'rails/all'
 require 'open-uri'
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,6 +13,7 @@ module StaffPlanner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
